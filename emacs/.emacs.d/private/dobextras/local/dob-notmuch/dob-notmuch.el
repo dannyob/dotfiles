f096a8127bffb15580e9b78ff6846d81f0fb952e
Danny O'Brien <danny@spesh.com>
2019-11-24 20:07:03 -0800

    spacemacs: Set up for machine-learning on incoming email.

--- emacs/.emacs.d/private/dobextras/local/dob-notmuch/dob-notmuch.el
+++ emacs/.emacs.d/private/dobextras/local/dob-notmuch/dob-notmuch.el
@@ -58,6 +58,35 @@ for sorting by author."
   (interactive)
   (notmuch-search-filter (format "from:\"%s\"" (notmuch-search-find-authors))))
 
+(defun dob-notmuch-ml-tag (iur)
+  "Tag for future machine-learning. Takes three numerical attributes between 0-5:
+   * Importance -- how bad would it be if I didn't see this
+   * Urgency    -- how quickly should I be shown this?
+   * Relevance  -- how connected is this to my actual work?
+"
+  (interactive "sImportance/Urgency/Relevance:")
+  (if (string-equal iur "spam") (setq iur "000"))
+  (let ((importance (string-to-number (substring iur 0 1)))
+        (urgency (string-to-number (substring iur 1 2)))
+        (relevance (string-to-number (substring iur 2 3))))
+   (notmuch-show-tag-message
+    (concat "+importance=" (number-to-string importance))
+    (concat "+urgency=" (number-to-string urgency))
+    (concat "+relevance=" (number-to-string relevance)))
+   (if (= 0 (or importance urgency relevance))
+      (dob-notmuch-spamify))))
+
+;; (defun dob--debug (x)
+;;   (print (plist-get x :body)))
+
+;; (defun dob-notmuch-extract-people ()
+;;   "Use polyglot to extract the names of people from an email, and feed that as choices to dob-make-person"
+;;   (interactive)
+;;   (notmuch-show-with-message-as-text 'dob--debug))
+    
+;;          (call-shell-region (buffer-end 0) (buffer-end 1) "polyglot ner" nil "foo")))))
+
+
 (use-package notmuch
   :bind (("C-c t" . dob-notmuch-today)
          ("C-c n" . dob-notmuch-now)
@@ -66,6 +95,7 @@ for sorting by author."
          ("S"     . dob-notmuch-spamify)
          ("F"     . dob-limit-to-author)
          :map notmuch-show-mode-map
+         ("T"     . dob-notmuch-ml-tag)
          ("S"     . dob-notmuch-show-spamify-message-then-next-or-next-thread)))
 
 (evil-define-key 'normal notmuch-message-mode-map (kbd "ZZ") 'notmuch-draft-save)
