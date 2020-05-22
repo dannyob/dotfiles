922534bb9a8323b40d06cc32d980d84c433f7e6c
Danny O'Brien <danny@spesh.com>
2020-05-22 16:05:40 -0700

    guix: Beginnings of a manifest setup.

---
+++ guix/.config/guix-profiles/manifests.scm
@@ -0,0 +1,92 @@
+(use-modules (gnu packages))
+
+(define profile-directory (string-append (getenv "HOME") "/.config/guix-profiles/"))
+
+;; base
+(define base-manifest
+  (specifications->manifest
+   '(
+      "aspell"
+      "aspell-dict-en"
+      "binutils"
+      "coreutils"
+      "cryptsetup"
+      "curl"
+      "emacs"
+      "emacs-guix"
+      "fasd"
+      "file"
+      "git"
+      "glibc-utf8-locales"
+      "gnupg"
+      "guile"
+      "guile-colorized"
+      "guile-json"
+      "guile-readline"
+      "guile-sqlite3"
+      "imagemagick"
+      "jq"
+      "lsof"
+      "magic-wormhole"
+      "nmap"
+      "neovim"
+      "nss"
+      "nss-certs"
+      "openssh"
+      "owncloud-client"
+      "patchelf"
+      "pinentry"
+      "python"
+      "rsync"
+      "sed"
+      "sqlite"
+      "stow"
+      "strace"
+      "the-silver-searcher"
+      "tidy"
+      "tor"
+      "unzip"
+      "wget"
+      "xdg-user-dirs"
+      "xdg-utils"
+      "zip"
+      "zsh")))
+
+;; laptop tools
+(define laptop-manifest
+   (specifications->manifest
+      '(
+         "bluez"
+         "powertop")))
+
+;; stuff I need for a decent desktop
+(define desktop-manifest
+   (specifications->manifest
+      '(
+         "pulseaudio"
+         "pavucontrol"
+         "kitty"
+         "hexchat"
+         "font-google-noto"
+         "font-dejavu"
+         "font-iosevka-term"
+         "fontconfig"
+         "feh"
+         "evince"
+         "xev"
+         "mako"
+         "docker"
+         "docker-cli"
+         "clementine"
+         "libreoffice"
+         "youtube-dl"
+         "xhost"
+         "dmenu"
+         "sway"
+         "wl-clipboard")))
+
+(define manifest-assoc `(("desktop" . ,desktop-manifest)
+                         ("laptop" . ,laptop-manifest)
+                         ("base" . ,base-manifest)))
+
+;; INIT -- create profiles
