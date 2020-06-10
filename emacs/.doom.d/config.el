739dbcd9396227832394c652fd6ee4943aad291f
Danny O'Brien <danny@spesh.com>
2020-06-09 17:53:07 -0700

    doomemacs: Fix bad merge.

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -120,14 +120,14 @@
 
 ;; (setq spaceline-org-clock-p t)          ;
 
-;; (defun dob-begin ()
-;;   "Start up my day"
-;;   (interactive)
-;;   (switch-to-buffer "daylog.org")
-;;   (set-window-dedicated-p nil t)
-;;   (org-id-goto "ac128a00-0af4-43e5-942e-38a2f36afd28")
-;;   (split-window-horizontally)
-;;   (dob-notmuch-now))
+(defun dob-begin ()
+   "Start up my day"
+   (interactive)
+   (switch-to-buffer "daylog.org")
+   (set-window-dedicated-p nil t)
+   (org-id-goto "ac128a00-0af4-43e5-942e-38a2f36afd28")
+   (split-window-horizontally)
+   (dob-notmuch-now))
 
 ;; (defun dob-person-filename (person-name)
 ;;   (let* ((name-file  (downcase (replace-regexp-in-string "[^[:alnum:]]" "-" (string-trim person-name))))
@@ -318,7 +318,6 @@ for sorting by author."
 (require 'ol-info)
 (require 'ol-eww)
 
-<<<<<<< HEAD
 (after! org
 ;;  (setq org-journal-dir "~/Private/wiki/journal/")
 ;;  (setq org-journal-file-type 'weekly)
@@ -381,66 +380,3 @@ for sorting by author."
     "Insert a timestamp with today's time and date."
     (interactive "P")
     (org-time-stamp '(16))))
-=======
-(setq org-log-done 'time)
-(setq org-journal-dir "~/Private/wiki/journal/")
-(setq org-journal-file-type 'weekly)
-(setq org-journal-file-format "%Y-%m-%d.org")
-(defun org-journal-date-format-func (time)
-  "Custom function to insert journal date header.
-
-  When buffer is empty prepend a header in front the entry header."
-  (concat (when (= (buffer-size) 0)
-            (concat
-             (pcase org-journal-file-type
-               (`daily "#+TITLE: Daily Journal\n")
-               (`weekly "#+TITLE: Weekly Journal\n")
-               (`monthly "#+TITLE: Monthly Journal\n")
-               (`yearly "#+TITLE: Yearly Journal\n"))))
-          org-journal-date-prefix
-          (format-time-string "%A, %F" time)))
-(setq org-journal-date-format 'org-journal-date-format-func)
-(add-to-list 'org-agenda-files org-journal-dir)
-
-(defun org-journal-find-location ()
-  ;; Open today's journal, but specify a non-nil prefix argument in order to
-  ;; inhibit inserting the heading; org-capture will insert the heading.
-  (org-journal-new-entry t)
-  ;; Position point on the journal's top-level heading so that org-capture
-  ;; will add the new entry as a child entry.
-  (goto-char (point-min)))
-
-(setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/Private/org/todo.org" "Inbox")
-         "* TODO %?\n  %i\n  %a")
-        ("n" "Note" entry (file "~/Private/wiki/notes.org")
-         "* %? %^g\nEntered on %U\n  %i\n  %a")
-        ("j" "Journal entry" entry (function
-                                    org-journal-find-location)
-         "* %(format-time-string org-journal-time-format)%^{Title}\n%i%?")))
-
-(if (string-equal "yacht" (getenv "SHORTHOST"))
-    (setq org-capture-templates
-          '(("t" "Todo" entry (file+headline "~/Private/org/yacht.org" "Inbox")
-             "* TODO %?\n  %i\n  %a")
-            ("n" "Note" entry (file "~/Private/wiki/notes.org")
-             "* %? %^g\nEntered on %U\n  %i\n  %a"))))
-
-(setq org-todo-keywords
-      '((sequence "TODO" "WAITING" "|" "CANCELED" "DONE" "DELEGATED")))
-
-(setq org-startup-indented t
-      org-bullets-bullet-list '(" ")
-      org-ellipsis "  "
-      org-pretty-entities t
-      org-hide-emphasis-markers t
-      org-agenda-block-separator ""
-      org-fontify-whole-heading-line t
-      org-fontify-done-headline t
-      org-fontify-quote-and-verse-blocks t)
-
-(defun dob-org-insert-time-now (arg)
-   "Insert a timestamp with today's time and date."
-   (interactive "P")
-   (org-time-stamp '(16)))
->>>>>>> d8fd7e797649953dde3602674705a530c06ec381
