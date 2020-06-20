dc5326b78a0365c806dc8e1c8d3374fe0a95fe61
Danny O'Brien <danny@codetherapy.space>
2020-06-20 12:20:12 -0700

    doomemacs: Can't even get my name right.

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -3,6 +3,11 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+;;
+(setq user-full-name "Danny O'Brien"
+      user-mail-address "danny@spesh.com")
 
 ;; If we have native compilation, let's get compiling!
 (when (fboundp 'native-compile-async)
@@ -11,10 +16,6 @@
             comp-deferred-compilation-black-list
             '("autoloads.el")))
 
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-(setq user-full-name "Danny O'Brien"
-      user-mail-address "danny@spesh..com")
 
 ;; are the three important ones:
 ;;
