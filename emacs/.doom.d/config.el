abe551f33fe554316e1b1ac2656a74d413cf713e
Danny O'Brien <danny@spesh.com>
2020-07-18 21:05:59 -0700

    doomemacs: First steps for coping with HiDPI.

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -30,9 +30,10 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Iosevka" :size 32))
+(defvar dob-hidpi 2 "Scaling factor for HiDPI monitors")
+(setq doom-font (font-spec :family "Iosevka" :size (* 16 dob-hidpi)))
 (setq doom-variable-pitch-font (font-spec :family "Iosevka Aile"))
-(setq doom-big-font (font-spec :family "Iosevka Aile" :size 48))
+(setq doom-big-font (font-spec :family "Iosevka Aile" :size (* 24 dob-hidpi)))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
