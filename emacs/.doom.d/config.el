35bfd508ddc9d4e025365ab78dfacef092039012
Danny O'Brien <danny@eff.org>
2020-11-20 02:05:00 -0800

    doomemacs: Turn on org-crypt, and configure it.

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -302,7 +302,12 @@
   (require 'ol-eww)
   (require 'org-ql)
   (require 'org-attach)
-
+  (require 'org-crypt)
+  (org-crypt-use-before-save-magic)
+  (setq org-tags-exclude-from-inheritance (quote ("crypt")))
+;; GPG key to use for encryption
+;; Either the Key ID or set to nil to use symmetric encryption.
+  (setq org-crypt-key "E1E70D6E64BA8D1F74E78285E5001906A3FDE45E")
   (setq org-startup-indented t
         org-todo-keywords '((sequence "TODO" "WAITING" "|" "CANCELED" "DONE" "DELEGATED"))
         org-bullets-bullet-list '(" ")
