c2f658ddcb96a38341a3c47edf0a00c565d94d81
Danny O'Brien <danny@eff.org>
2020-11-20 02:06:25 -0800

    doomemacs: Disable unused org-roam plugin.

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -138,7 +138,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +pandoc +roam)
+       (org +pandoc)
        ;;perl              ; write code no one else can comprehend
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
