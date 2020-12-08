97199a10efb776d42935394e9bd994302969a254
Danny O'Brien <danny@eff.org>
2020-12-07 21:16:57 -0800

    mu4e: Add threading.

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -254,6 +254,97 @@
   ;; gnus-inherited email viewer
   (setq mu4e-view-use-gnus 't)
 
+
+  ;; Funky Thread Folding!
+  ;; From: https://gist.github.com/felipeochoa/614308ac9d2c671a5830eb7847985202
+  ;;
+(defun mu4e~headers-msg-unread-p (msg)
+  "Check if MSG is unread."
+  (let ((flags (mu4e-message-field msg :flags)))
+    (and (member 'unread flags) (not (member 'trashed flags)))))
+
+(defvar mu4e-headers-folding-slug-function
+  (lambda (headers) (format " (%d)" (length headers)))
+  "Function to call to generate the slug that will be appended to folded threads.
+This function receives a single argument HEADERS, which is a list
+of headers about to be folded.")
+
+(defun mu4e~headers-folded-slug (headers)
+  "Generate a string to append to the message line indicating the fold status.
+HEADERS is a list with the messages being folded (including the root header)."
+  (funcall mu4e-headers-folding-slug-function headers))
+
+(defun mu4e~headers-fold-make-overlay (beg end headers)
+  "Hides text between BEG and END using an overlay.
+HEADERS is a list with the messages being folded (including the root header)."
+  (let ((o (make-overlay beg end)))
+    (overlay-put o 'mu4e-folded-thread t)
+    (overlay-put o 'face '(background-color . "#FF0000" ))
+    ;; (overlay-put o 'display (concat (mu4e~headers-folded-slug headers) "\n"))
+    (overlay-put o 'evaporate t)
+    (overlay-put o 'invisible t)))
+
+(defun mu4e~headers-fold-find-overlay (loc)
+  "Find and return the 'mu4e-folded-thread overlay at LOC, or return nil."
+  (cl-dolist (o (overlays-in (1- loc) (1+ loc)))
+    (when (overlay-get o 'mu4e-folded-thread)
+      (cl-return o))))
+
+(defun mu4e-headers-fold-all ()
+  "Fold all the threads in the current view."
+  (interactive)
+  (let ((thread-id "") msgs fold-start fold-end)
+    (mu4e-headers-for-each
+     (lambda (msg)
+       (end-of-line)
+       (push msg msgs)
+       (let ((this-thread-id (mu4e~headers-get-thread-info msg 'thread-id)))
+         (if (string= thread-id this-thread-id)
+             (setq fold-end (point))
+           (when (< 1 (length msgs))
+             (mu4e~headers-fold-make-overlay fold-start fold-end (nreverse msgs)))
+           (setq fold-start (point)
+                 fold-end (point)
+                 msgs nil
+                 thread-id this-thread-id)))))
+    (when (< 1 (length msgs))
+      (mu4e~headers-fold-make-overlay fold-start fold-end (nreverse msgs)))))
+
+(defun mu4e-headers-toggle-thread-folding (&optional subthread)
+  "Toggle the folding state for the thread at point.
+If SUBTHREAD is non-nil, only fold the current subthread."
+  ;; Folding is accomplished using an overlay that starts at the end
+  ;; of the parent line and ends at the end of the last descendant
+  ;; line. If there's no overlay, it means it isn't folded
+  (interactive "P")
+  (if-let ((o (mu4e~headers-fold-find-overlay (point-at-eol))))
+      (delete-overlay o)
+    (let* ((msg (mu4e-message-at-point))
+           (thread-id (mu4e~headers-get-thread-info msg 'thread-id))
+           (path-re (concat "^" (mu4e~headers-get-thread-info msg 'path)))
+           msgs first-marked-point last-marked-point)
+      (mu4e-headers-for-each
+       (lambda (submsg)
+         (when (and (string= thread-id (mu4e~headers-get-thread-info submsg 'thread-id))
+                    (or (not subthread)
+                        (string-match-p path-re (mu4e~headers-get-thread-info submsg 'path))))
+           (push msg msgs)
+           (setq last-marked-point (point-at-eol))
+           (unless first-marked-point
+             (setq first-marked-point last-marked-point)))))
+      (when (< 1 (length msgs))
+        (mu4e~headers-fold-make-overlay first-marked-point last-marked-point (nreverse msgs))))))
+
+(map!
+   :map (mu4e-headers-mode-map)
+   :n "za" 'mu4e-headers-toggle-thread-folding
+   :n "zM" 'mu4e-headers-fold-all)
+
+;; Below is more evilly correct but doesn't work with fold module in doom-emacs
+;; because fold module remaps evil-fold-action keys.
+;; (setq evil-fold-list (cl-remove-if (lambda (e) (eq (caar e) 'mu4e-headers-mode)) evil-fold-list))
+;; (add-to-list 'evil-fold-list `((mu4e-headers-mode) :open-all nil :close-all ,(lambda () (call-interactively 'mu4e-headers-fold-all)) :toggle ,(lambda () (call-interactively 'mu4e-headers-toggle-thread-folding)) :open nil :open-rec  nil :close nil))
+
   ;; Colorize headers based on tags
   ;;
 
