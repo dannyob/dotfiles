a7881d5bc2cd7c3dcafe8850d1e1f76c7843f5b5
Danny O'Brien <danny@eff.org>
2021-01-09 02:23:58 -0800

    emacs: Mysterious theme safety.

--- emacs/.doom.d/custom.el
+++ emacs/.doom.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("79278310dd6cacf2d2f491063c4ab8b129fee2a498e4c25912ddaa6c3c5b621e" default))
+   '("0c6a36393d5782839b88e4bf932f20155cb4321242ce75dc587b4f564cb63d90" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "79278310dd6cacf2d2f491063c4ab8b129fee2a498e4c25912ddaa6c3c5b621e" default))
  '(safe-local-variable-values
    '((eval cl-flet
            ((enhance-imenu-lisp
