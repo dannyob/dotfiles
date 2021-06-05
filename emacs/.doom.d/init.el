cb61b7a69cc9e2cef7233d1c2e18d14d933c8ea3
Danny O'Brien <danny@eff.org>
2021-06-05 16:03:34 -0700

    doom: Add org-roam.

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -139,7 +139,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +pandoc)
+       (org +pandoc +roam)
        ;;perl              ; write code no one else can comprehend
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
