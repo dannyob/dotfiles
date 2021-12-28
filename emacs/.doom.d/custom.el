a0ec9313af554f3e57536d57290b6a7015765e31
Danny O'Brien <danny@codetherapy.space>
2021-12-28 14:50:06 -0800

    doom: Ignore customization file.

--- emacs/.doom.d/custom.el
+++ emacs/.doom.d/custom.el
@@ -6,7 +6,31 @@
  '(custom-safe-themes
    '("e8567ee21a39c68dbf20e40d29a0f6c1c05681935a41e206f142ab83126153ca" "2ce76d65a813fae8cfee5c207f46f2a256bac69dacbb096051a7a8651aa252b0" "0c6a36393d5782839b88e4bf932f20155cb4321242ce75dc587b4f564cb63d90" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "79278310dd6cacf2d2f491063c4ab8b129fee2a498e4c25912ddaa6c3c5b621e" default))
  '(safe-local-variable-values
-   '((eval modify-syntax-entry 43 "'")
+   '((eval cl-flet
+           ((enhance-imenu-lisp
+             (&rest keywords)
+             (dolist
+                 (keyword keywords)
+               (add-to-list 'lisp-imenu-generic-expression
+                            (list
+                             (purecopy
+                              (concat
+                               (capitalize keyword)
+                               (if
+                                   (string=
+                                    (substring-no-properties keyword -1)
+                                    "s")
+                                   "es" "s")))
+                             (purecopy
+                              (concat "^\\s-*("
+                                      (regexp-opt
+                                       (list
+                                        (concat "define-" keyword))
+                                       t)
+                                      "\\s-+\\(" lisp-mode-symbol-regexp "\\)"))
+                             2)))))
+           (enhance-imenu-lisp "bookmarklet-command" "class" "command" "ffi-method" "function" "mode" "parenscript" "user-class"))
+     (eval modify-syntax-entry 43 "'")
      (eval modify-syntax-entry 36 "'")
      (eval modify-syntax-entry 126 "'")
      (eval let
@@ -60,3 +84,5 @@
  ;; If there is more than one, they won't work right.
  '(mode-line ((t (:height 80))))
  '(mode-line-inactive ((t (:height 80)))))
+(put 'projectile-ag 'disabled nil)
+(put 'projectile-grep 'disabled nil)
