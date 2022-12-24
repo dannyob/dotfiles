1db30e7cdd701cb03d972e5dd63bdb6b8e78232b
Danny O'Brien <danny@spesh.com>
2022-12-24 11:05:56 -0800

    doomemancs: Refactor dob-org-push

--- emacs/.doom.d/lisp/dob-org-push.el
+++ emacs/.doom.d/lisp/dob-org-push.el
@@ -28,13 +28,17 @@
 (require 'org)
 (require 'org-ql)
 (require 'plz)
+(require 'doom)
+
+(defconst dop-slack-filter-filename (concat (or doom-user-dir (file-name-directory load-file-name))
+                                            "lisp/dob-org-push-pandoc-slack-filter.lua"))
 
 (defun dop--slack-secret-token ()
   "Obtain our slack token from authinfo."
   (funcall (plist-get (car (auth-source-search :host "slack.com" :user "token")) :secret)))
 
 (defun dop-subtree ()
-  "Push current tree based on PUSH property URI."
+  "Push current tree to destination defined by PUSH property URI."
   (let* ((sync-dest (cdr (assoc "PUSH" (org-entry-properties))))
          (parsed-sync-url (url-generic-parse-url sync-dest))
          (sync-type (url-type parsed-sync-url)))
@@ -48,30 +52,12 @@ Channel and optionally message timestamp is given by URL."
       (org-narrow-to-element)
       (let ((slack-buf (generate-new-buffer "*slackbuf*")))
         (call-process-region (point-min) (point-max)
-                             "pandoc" nil slack-buf nil "-f" "org" "-t" "~/.doom.d/lisp/dob-org-push-pandoc-slack-filter.lua")
+                             "pandoc" nil slack-buf nil "-f" "org" "-t" dop-slack-filter-filename)
         (widen)
         (org-set-property "PUSH" (if (url-target url)
                                      (dop-slack-update-message url slack-buf)
                                    (dop-slack-post-message url slack-buf)))
       (kill-buffer slack-buf))))
-
-(defun dop-slack-delete-message (url)
-  "Delete Slack message pointed to by URL.
-Returns an URL with channel but no ID."
-  (let* ((slack-channel (s-chop-suffix "/" (s-chop-prefix "/" (car (url-path-and-query url)))))
-         (addr "https://slack.com/api/chat.delete")
-         (token (concat "Bearer " (dop--slack-secret-token)))
-         (ts (url-target url))
-         (payload `(("channel" . ,slack-channel)
-                    ("ts" . ,ts)))
-         (response (plz 'post addr
-                     :headers `(("Content-Type" . "application/json; charset=utf-8")
-                                ("Authorization" . ,token))
-                     :body (json-encode payload)
-                     :as #'json-read)))
-    (message (json-encode response))
-    (setf (url-target url) nil)
-    (url-recreate-url url)))
 
 (defun dop-slack-exec-with-url (url command payload)
   "Execute Slack API COMMAND with PAYLOAD and channel, ts from URL."
@@ -81,7 +67,8 @@ Returns an URL with channel but no ID."
                        (dop-slack-exec command (append `(("channel" . ,slack-channel)
                                                                   ("ts" . ,ts))
                                                                 payload))
-                     (dop-slack-exec command (append `(("channel" . ,slack-channel)))))))
+                     (dop-slack-exec command (append `(("channel" . ,slack-channel))
+                                                     payload)))))
     response))
 
 (defun dop-slack-exec (command payload)
@@ -93,67 +80,44 @@ Returns an URL with channel but no ID."
                                 ("Authorization" . ,token))
                      :body (json-encode payload)
                      :as #'json-read)))
+    (message (json-encode payload))
+    (message (json-encode response))
     response))
-
 
 (defun dop-slack-join-channel (url)
   "Joins a Slack channel, defined by URL.
 Necessary before a bot can post to the channel (unless
 you're using webhooks, which we are not.)"
-  (let* ((slack-channel (s-chop-suffix "/" (s-chop-prefix "/" (car (url-path-and-query url))))  )
-         (addr "https://slack.com/api/conversations.join")
-         (token (concat "Bearer " (dop--slack-secret-token)))
-         (payload `(("channel" . ,slack-channel)))
-         (response (plz 'post addr
-                     :headers `(("Content-Type" . "application/json; charset=utf-8")
-                                ("Authorization" . ,token))
-                     :body (json-encode payload)
-                     :as #'json-read)))
-    (message (json-encode response))
-    response))
+  (dop-slack-exec-with-url url "conversations.join" '()))
 
 (defun dop-slack-update-message (url slack-buf)
   "Change an existing message, pointed to by URL, to text in SLACK-BUF.
 Requires that the URL include a message id in its fragment (or target) field."
   (dop-slack-join-channel url)
   (with-current-buffer slack-buf
-    (let* ((slack-channel (s-chop-suffix "/" (s-chop-prefix "/" (car (url-path-and-query url))))  )
-           (addr "https://slack.com/api/chat.update")
-           (token (concat "Bearer " (dop--slack-secret-token)))
-           (payload `(("channel" . ,slack-channel)
-                      ("ts" . ,(url-target url))
-                      ("text" . ,(buffer-string))))
-           (response (plz 'post addr
-                       :headers `(("Content-Type" . "application/json; charset=utf-8")
-                                  ("Authorization" . ,token))
-                       :body (json-encode payload)
-                       :as #'json-read)))
-      (message (json-encode response))
-      (url-recreate-url url))))
+    (dop-slack-exec-with-url url "chat.update" `(("text" . ,(buffer-string))))
+    (url-recreate-url url)))
 
 (defun dop-slack-post-message (url slack-buf)
   "Create a message in channel defined by URL, containing contents of SLACK-BUF."
   (dop-slack-join-channel url)
   (with-current-buffer slack-buf
-    (let* ((slack-channel (s-chop-suffix "/" (s-chop-prefix "/" (car (url-path-and-query url))))  )
-           (addr "https://slack.com/api/chat.postMessage")
-           (token (concat "Bearer " (dop--slack-secret-token)))
-           (payload `(("channel" . ,slack-channel)
-                      ("text" . ,(buffer-string))))
-           (response (plz 'post addr
-                       :headers `(("Content-Type" . "application/json; charset=utf-8")
-                                  ("Authorization" . ,token))
-                       :body (json-encode payload)
-                       :as #'json-read)))
-      (message (json-encode response))
+    (message (buffer-string))
+    (let ((response (dop-slack-exec-with-url url "chat.postMessage" `(("text" . ,(buffer-string))))))
       (concat (url-recreate-url url) "#" (alist-get 'ts response)))))
+
+(defun dop-slack-delete-message(url)
+  "Delete Slack message pointed to by URL.
+Returns an URL with channel but no ID."
+  (dop-slack-exec-with-url url "chat.delete" '())
+  (setf (url-target url) nil)
+  (url-recreate-url url))
 
 (defun dop-buffer ()
   "Push all nodes in current buffer that have PUSH property set."
   (interactive)
   (org-ql-select (current-buffer) '(property "PUSH")
     :action #'dop-subtree))
-
 
 (provide 'dob-org-push)
 ;; (setf (alist-get "dob-org-push" package-lint--allowed-prefix-mappings) "dop")
