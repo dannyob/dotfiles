212edf370fa7d745233b3387bc73a049cde00dea
Danny O'Brien <danny@spesh.com>
2024-11-15 20:28:48 +0700

    Custom blather

--- emacs/.config/doom/config.el
+++ emacs/.config/doom/config.el
@@ -778,22 +778,5 @@ locally."
     response))
 
 
-;; Finally, I like a teeny modeline
-(setq doom-modeline-height 1)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(mode-line ((t (:height 80))))
- '(mode-line-inactive ((t (:height 80)))))
-
 ;; NOW IT IS TIME FOR THE CUSTOMARY CUSTOMIZATION
 ;;
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("79278310dd6cacf2d2f491063c4ab8b129fee2a498e4c25912ddaa6c3c5b621e" default)))
